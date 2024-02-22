1c1c93e55a20232a6708aeced616152a9291ab01
Aereyelle <105485264+Aereyelle@users.noreply.github.com>
2024-02-22 18:28:10 +0000

    Add files via upload

---
+++ details.py
@@ -0,0 +1,14 @@
+"""
+I will use an input command to ask the user's name and assign it to a variable
+I will use an input command to ask the user's age and assign it to a variable
+I will use an input command to ask the user's house number and assign it to a variable
+I will use an input command to ask the user's street name and assign it to a variable
+I will print out an f print sentence with the variables included
+"""
+
+name=input("What is your name? ")
+age=input("How old are you? ")
+house_num=input("What is your house number? ")
+street=input("What is your street called? ")
+
+print(f"This is {name}. They are {age} years old and live at house number {house_num} on {street}.")
